a725bddced51f6cdd3832ad58b7ed8670020613f
“Zuzana <zuzana@getyourguide.com>
2023-03-11 15:04:39 +0100

    added bg

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
-function setup() {
 
+let bg;
+
+function preload() {
+    bg = loadImage("img/bg-withbanner.png");
+}
+
+function setup() {
+    image(bg, 0, 0);
+    canvas = createCanvas(1024, 710);
 }
 
 function draw() {
-    
+    image(bg,0, 0);
 }
+
+
